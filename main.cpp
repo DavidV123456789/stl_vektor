af7f74826472229ad7d7f86a12b1b8b46cf10e16
map <1david.vdoviak>
2022-02-15 13:17:07 +0100

    q

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 #include <iterator>//pre pouzitie iteratorov
 #include <algorithm>//pre pouzitie algoritmov
 #include "stlVector.h"
-
+#include <fstream>
 using std::cout;
 using std::endl;
 using std::vector;
+
+template<typename  T>
+void  print(const T &kontajner, const char *sprava)
+{
+
+    cout<<sprava<<": ";
+    typename  T::const_iterator pos;
+    for(pos=kontajner.begin();pos!=kontajner.end();pos++)
+    {
+        cout<<*pos<<" ";
+    }
+    cout<<endl;
+}
 
 int main() {
     //std::cout << "Hello, World!" << std::endl;
@@ -47,6 +60,8 @@ int main() {
 
     //vypis prvkov vektora pomocou  algoritmu: //
     //auto kam=std::ostream_iterator<int>(cout," ");//mozeme nahradit: std::copy(intCisla.begin(),intCisla.end(),kam);//rovnako aj z ostatnymi dvomi
+    auto kam=std::ostream_iterator<int>(cout," ");
+    //
     std::copy(intCisla.begin(),intCisla.end(),std::ostream_iterator<int>(cout," "));
     cout<<endl;
 
@@ -92,7 +107,7 @@ int main() {
     int delitel=3;
     cout<<std::count_if(intCisla.begin(),intCisla.end(),[delitel](auto i){return i%delitel==0 && i!=0;});//premenne musi zachytavat do []
     cout<<endl;
-
+/*
     auto koniec=std::remove(intCisla.begin(),intCisla.end(),5);//musi sa zmenit koniec ak odstranujem priamo hodnoty
     std::copy(intCisla.begin(),koniec,std::ostream_iterator<int>(cout," "));
     cout<<endl;
@@ -100,7 +115,33 @@ int main() {
     koniec=std::remove_if(intCisla.begin(),koniec,[](auto i){return i*i>5;});
     std::copy(intCisla.begin(),koniec,std::ostream_iterator<int>(cout," "));
     cout<<endl;
+
     intCisla.erase(koniec,intCisla.end());//uplnne vymaze premenne od konca do praveho konca
+*/
+
+    std::copy_if(intCisla.begin(),intCisla.end(),kam,[](auto i)->bool{return i%2;});
+    cout<<endl;
+    std::remove_copy_if(intCisla.begin(),intCisla.end(),kam,[](auto i)->bool{return i%2;});
+    cout<<endl;
+    std::replace_copy_if(intCisla.begin(),intCisla.end(),kam,[](auto i)->bool{return i%2;},10);
+    cout<<endl;
+    std::replace(intCisla.begin(),intCisla.end(),8,88);
+    cout<<endl;
+    auto isPrvocislo=[](int cislo){int i=2;if(cislo==2){return 1;};for(i;i<=cislo/2;i++){if(cislo%i==0){return 0;}};return 1;};
+    std::copy_if(intCisla.begin(),intCisla.end(),kam,isPrvocislo);
+
+    std::for_each(intCisla.begin(),intCisla.end(),[](auto &i){i+=5;});
+    print(intCisla,"Vypis:");
+
+    std::vector<int>novy;
+    std::copy_if(intCisla.begin(),intCisla.end(),std::back_inserter(novy),[](auto i){return i>=15;});
+    print(novy,"Vypis:");
+
+    std::ofstream fout("zapis.txt");
+    std::copy_if(intCisla.begin(),intCisla.end(),std::ostream_iterator<int>(fout," "),[](auto i)->bool{return i%2;});
+    std::ifstream fin("citaj.txt");
+    std::copy(std::istream_iterator<int>(fin),std::istream_iterator<int>(),std::ostream_iterator<int>(cout,""));
 
     return 0;
 }
+
